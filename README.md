44b448762c84691fcdef9135f14bfbbc451ee72c
neil patel <neil@myindianweddingapp.com>
2014-10-29 10:29:16 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+== README
+
+Splash Page Demo 2014
+
+Rails 4.0.4
+
+Ruby 2.0
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splash::Application.config.secret_key_base = '3a894ec78c74e80ff71a5cb59942c93d34e2ebeb97018a23a5fa0eeb913d0eb2eda2ee31c7e96485a9e97a4512d2718071ec2e44bacdfdaaab59e0cd429eeb70'
